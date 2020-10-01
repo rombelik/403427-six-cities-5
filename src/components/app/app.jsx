5f2a0fb8fdd392a2946e7f484c2616581fbb4502
Roman Belik <dabonka@yandex.ru>
2020-10-01 22:56:43 +0300

    fixed syntax according to requirements

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import Main from '../Main';
+import React from 'react';
 import PropTypes from 'prop-types';
+import Main from '../main/main';
 
 const App = ({rentOffers}) => {
 
