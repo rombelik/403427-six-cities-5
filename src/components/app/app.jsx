08b37eb866bdcf0e87a3a7bae70e2525d4659faf
Roman Belik <dabonka@yandex.ru>
2020-10-06 17:02:52 +0300

    add routes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
 import Main from '../main/main';
+import SignIn from '../sign-in/sign-in';
+import Favorites from '../favorites/favorites';
+import Room from '../room/room';
+import Header from "../header/header";
 
 const App = ({rentOffers}) => {
 
   return (<React.Fragment>
-    <Main rentOffers={rentOffers} />
+    <Header />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main rentOffers={rentOffers} />
+        </Route>
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/offer/:id">
+          <Room />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </React.Fragment>);
+
 };
 
 App.propTypes = {
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,11 +1,74 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import PlacesForm from '../places-form/places-form';
+import PlacesCard from "../places-card/places-card";
+
 const Main = ({rentOffers}) => {
 
-  return (<React.Fragment>
-    <p>You have {rentOffers} offers.</p>
-  </React.Fragment>);
+  return (
+    <React.Fragment>
+      <main className="page__main page__main--index">
+        <h1 className="visually-hidden">Cities</h1>
+        <div className="tabs">
+          <section className="locations container">
+            <ul className="locations__list tabs__list">
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item" href="#">
+                  <span>Paris</span>
+                </a>
+              </li>
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item" href="#">
+                  <span>Cologne</span>
+                </a>
+              </li>
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item" href="#">
+                  <span>Brussels</span>
+                </a>
+              </li>
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item tabs__item--active">
+                  <span>Amsterdam</span>
+                </a>
+              </li>
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item" href="#">
+                  <span>Hamburg</span>
+                </a>
+              </li>
+              <li className="locations__item">
+                <a className="locations__item-link tabs__item" href="#">
+                  <span>Dusseldorf</span>
+                </a>
+              </li>
+            </ul>
+          </section>
+        </div>
+        <div className="cities">
+          <div className="cities__places-container container">
+            <section className="cities__places places">
+              <h2 className="visually-hidden">Places</h2>
+              <b className="places__found">{rentOffers} places to stay in Amsterdam</b>
+              <PlacesForm />
+              <div className="cities__places-list places__list tabs__content">
+                <PlacesCard />
+                <PlacesCard />
+                <PlacesCard />
+                <PlacesCard />
+
+              </div>
+            </section>
+            <div className="cities__right-section">
+              <section className="cities__map map" />
+            </div>
+          </div>
+        </div>
+      </main>
+    </React.Fragment>
+
+  );
 };
 
 Main.propTypes = {
